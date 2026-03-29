cdd19cdb2b74aa9be7c3df422d2624d85923e83d
Короленко Світлана Олегівна <s.korolenko@tk.sat.ua>
2026-03-30 01:34:31 +0300

    add slider

---
+++ js/slider.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.reviews_content', {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  autoplay: {
+    delay: 15000
+  },
+  breakpoints: {
+    320: { slidesPerView: 1, spaceBetween: 10 },
+    480: { slidesPerView: 1, spaceBetween: 10 },
+    768: { slidesPerView: 2, spaceBetween: 15 },
+    1024: { slidesPerView: 2, spaceBetween: 20 }
+  }
+});
